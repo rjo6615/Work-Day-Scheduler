4f70af11837e316c2f5c5ff7fe80b16ece64d0d3
rjo6615 <robertjobernier@gmail.com>
2022-10-16 16:25:22 -0400

    added arrays to store ids for timeblocks

--- script.js
+++ script.js
@@ -17,3 +17,9 @@ $.each(timeBlock, function () {
     $(this).next().addClass("future");
   }
 });
+
+//arrays for ids on timeblocks
+timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+varTimeId = ["#nineAm", "#tenAm", "#elevenAm", "#twelvePm", "#onePm", "#twoPm", "#threePm", "#fourPm", "#fivePm"];
+varLS = [nineAm, tenAm, elevenAm, twelvePm, onePm,  twoPm, threePm, fourPm, fivePm];
+arrayPos = 0;
