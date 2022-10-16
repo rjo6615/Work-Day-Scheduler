7cee69d1740d9c8fdcf59eb2e977c5321ee26ed8
rjo6615 <robertjobernier@gmail.com>
2022-10-16 16:27:05 -0400

    added function for save button

--- script.js
+++ script.js
@@ -23,3 +23,8 @@ timeArray = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 varTimeId = ["#nineAm", "#tenAm", "#elevenAm", "#twelvePm", "#onePm", "#twoPm", "#threePm", "#fourPm", "#fivePm"];
 varLS = [nineAm, tenAm, elevenAm, twelvePm, onePm,  twoPm, threePm, fourPm, fivePm];
 arrayPos = 0;
+
+$(".saveBtn").on("click", function (event) {
+  var schedulerItem = event.target.parentElement.previousElementSibling.children[0].value;
+  localStorage.setItem(event.target.attributes[0].value, schedulerItem);
+});
